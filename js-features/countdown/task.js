16c1d6b9bbf64c9d5c70bad43b8cc32b6b193929
Svetlana Konradi <sv.konradi87@gmail.com>
2020-05-20 01:32:35 +0400

    first task

---
+++ js-features/countdown/task.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let timer = () => {
+    const startTime = document.getElementById("timer");
+    let i = startTime.textContent--;
+    if (i == 1){
+        alert('Вы победили в конкурсе');
+        clearInterval(id);
+    }
+};
+let id = setInterval(timer, 1000);
