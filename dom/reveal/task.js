3855380da54ace602cfac3ea1dc0304c46f99b24
svetlana-konradi <53185747+svetlana-konradi@users.noreply.github.com>
2020-07-10 02:39:46 +0400

    Update task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,30 +1,18 @@
-let rotator = document.querySelector('.rotator');
-let rotatorCases = rotator.children;
+'use strict';
 
-const rotatorChange = function () {
-let i = 0;
-let nextElement = 0;
+document.addEventListener('scroll', function () {
+    let reveals = document.querySelectorAll('.reveal');
+    let viewportHeight = window.innerHeight;
 
-    while (i < rotatorCases.length) {
-        if (rotatorCases[i].classList.contains('rotator__case_active')) {
-            if (i !== rotatorCases.length - 1) {
-                nextElement = i + 1;
-            }
-            setActive(nextElement);
-            break;
-        }
-        i++;
-    }
-    function setActive(active) {
+   for (let i = 0; i < reveals.length; i++) {
+       let revealTop = reveals[i].getBoundingClientRect().top;
+       let revealBottom = reveals[i].getBoundingClientRect().bottom;
 
-        let index = [...rotatorCases].findIndex((element) => element.classList.contains('rotator__case_active'));
-        // у элементов с этим индексом убираем активный класс
-        rotatorCases[index].classList.remove('rotator__case_active');
-
-        //установить активный класс
-        rotatorCases[active].classList.add('rotator__case_active');
-    }
-
-};
-
-let timer = setInterval(rotatorChange, 1000);
+       if (revealTop < viewportHeight) {
+               reveals[i].classList.add('reveal_active');
+           }
+       if (revealBottom === 0) {
+           reveals[i].classList.remove('reveal_active');
+       }
+   }
+});
